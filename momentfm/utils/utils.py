da0c09cc8f8308bc6987d85888c166ad2187662d
ZHYMIR <dzhymir@gmail.com>
2025-12-09 13:57:35 -0500

    changed np.Inf to np.inf for compatibility to numpy 2.0.

--- momentfm/utils/utils.py
+++ momentfm/utils/utils.py
@@ -102,7 +102,7 @@ class EarlyStopping:
         self.counter = 0
         self.best_score = None
         self.early_stop = False
-        self.val_loss_min = np.Inf
+        self.val_loss_min = np.inf
         self.delta = delta
 
     def __call__(self, validation_loss):
